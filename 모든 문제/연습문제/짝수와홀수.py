fb5b9b6848abdf5232923bad1f67108331d97545
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-09-22 09:07:46 +0900

    Create 짝수와홀수.py

---
+++ 모든 문제/연습문제/짝수와홀수.py
@@ -0,0 +1,10 @@
+def solution(num):
+    # 삼항연산자 이용
+    answer = "" "Even" if num % 2 == 0 else "Odd"
+    
+    # if-else문 이용
+    # if num % 2 == 0 :
+    #     answer = "Even"
+    # else :
+    #     answer = "Odd"
+    return answer
