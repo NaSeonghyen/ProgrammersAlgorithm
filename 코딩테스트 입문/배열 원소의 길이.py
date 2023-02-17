9b6df91ed8c347cd97da33cdc10f0ce7e587eac7
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2023-02-17 17:01:06 +0900

    Update 배열 원소의 길이.py

--- 코딩테스트 입문/배열 원소의 길이.py
+++ 코딩테스트 입문/배열 원소의 길이.py
@@ -3,3 +3,8 @@ def solution(strlist):
     for i in strlist:
         list.append(len(i))
     return list
+
+# 다른 사람의 풀이
+# def solution(strlist):
+#     answer = list(map(len, strlist))
+#     return answer
