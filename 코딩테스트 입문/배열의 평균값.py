629f62d8b7c2e3e24514cbd9d1f2b9892395ddd7
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2023-02-17 13:55:32 +0900

    Update 배열의 평균값.py

--- 코딩테스트 입문/배열의 평균값.py
+++ 코딩테스트 입문/배열의 평균값.py
@@ -1,10 +1,10 @@
 def solution(numbers):
     # 첫번째 풀이
-    # sum = 0
-    # for i in numbers:
-    #     sum += i
-    # return sum/len(numbers)
+    sum = 0
+    for i in numbers:
+        sum += i
+    return sum/len(numbers)
     
     # 두번째 풀이(참고)
-    import numpy as np
-    return np.mean(numbers)
+    # import numpy as np
+    # return np.mean(numbers)
